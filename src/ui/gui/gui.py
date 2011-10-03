b8c5d03f1ee9d8beb8e76a82962c7d9074b71ecd
Abhijit Mahabal <amahabal@gmail.com>
2011-10-02 22:28:07 -0400

    Added configuration

--- src/ui/gui/gui.py
+++ src/ui/gui/gui.py
@@ -1,24 +1,41 @@
 from Tkinter import Tk, Button, Canvas, Frame, TOP, LEFT
 import sys
 
+import ConfigParser
+
 class GUI(object):
   """Tkinter-based UI for Seqsee."""
-  
+
   def __init__(self, run_state):
     print "Will run GUI"
-    
+    config = ConfigParser.SafeConfigParser()
+    config.read('config/gui.config')
+
+    geometry = config.get('App', 'geometry')
     mw = Tk()
-    mw.geometry('810x700+0+0')
-    self.SetupWindows(mw, run_state)
+    mw.geometry(geometry)
+    self.SetupWindows(mw, config, run_state)
     mw.mainloop()
 
-  def SetupWindows(self, mw, run_state):
+  def SetupWindows(self, mw, config, run_state):
     button_frame = Frame(mw)
     button_frame.pack(side=TOP)
-    Button(button_frame, text="Quit", command=self.Quit).pack(side=LEFT)
-    canvas = self.canvas = Canvas(mw, width=800, height=600,
-                                  bg='#EEEEFF')
+    self.SetupButtons(button_frame)
+
+    canvas_config = dict(config.items('Canvas'))
+    canvas = self.canvas = Canvas(mw, **canvas_config)
     canvas.pack(side=TOP)
+
+  def SetupButtons(self, button_frame):
+    Button(button_frame, text="Start", command=self.Start).pack(side=LEFT)
+    Button(button_frame, text="Pause", command=self.Pause).pack(side=LEFT)
+    Button(button_frame, text="Quit", command=self.Quit).pack(side=LEFT)
+
+  def Start(self):
+    pass
+
+  def Pause(self):
+    pass
 
   def Quit(self):
     sys.exit(1)
