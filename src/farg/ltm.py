52b6a0e48b538c9bdc29b9be72bd487c59c89214
Abhijit Mahabal <amahabal@gmail.com>
2011-11-04 22:52:05 -0400

    I have a complex system that seems to work, but I will have to test it a lot more to be convinced.
    If it works, it is way cleaner than the Perl version.

--- src/farg/ltm.py
+++ src/farg/ltm.py
@@ -2,28 +2,90 @@
 
 import cPickle as pickle
 
-class LTM(object):
-  def __init__(self, filename):
-    self.nodes = []
-    self.filename = filename
-    with open(filename) as f:
-      up = pickle.Unpickler(f)
-      self.Load(up)
+class LTMNode(object):
+  """Represents a node in the graph of LTM.
+  
+  Each node has a content --- an instance of some python class that implements Caching by using a
+  Create() method where the following is guarenteed for any instance inst:
 
-  def Load(self, unpickler):
+  inst == inst.__class__.Create(**inst.__dict__)
+  """
+
+  def __init__(self, content):
+    self.content = content
+
+  def __getstate__(self):
+    content = self.content
+    return (content.__class__, content.__dict__)
+
+  def __setstate__(self, state):
+    clsname, instance_dict = state
+    LTM.Unmangle(instance_dict)
+    self.content = clsname.Create(**instance_dict)
+
+class LTM(object):
+  def __init__(self, nodes_filename, edges_filename):
+    self.nodes = []
+    self.edges = []
+    self.nodes_filename = nodes_filename
+    self.edges_filename = edges_filename
+    with open(nodes_filename) as f:
+      up = pickle.Unpickler(f)
+      self.LoadNodes(up)
+    with open(edges_filename) as f:
+      up = pickle.Unpickler(f)
+      self.LoadEdges(up)
+
+  @staticmethod
+  def Mangle(content_dict, content_to_node_map):
+    """Replaces references to nodes with the nodes themselves."""
+    for k, v in content_dict.iteritems():
+      if v in content_to_node_map:
+        content_dict[k] = content_to_node_map[v]
+
+  @staticmethod
+  def Unmangle(content_dict):
+    for k, v in content_dict.iteritems():
+      if isinstance(v, LTMNode):
+        content_dict[k] = v.content
+
+  def LoadNodes(self, unpickler):
     while True:
       try:
         node = unpickler.load()
+        print "Loaded node content=", node.content
         self.AddNode(node)
+      except EOFError:
+        print "Done reading"
+        break
+    for node in self.nodes:
+      LTM.Unmangle(node.content.__dict__)
+
+
+  def LoadEdges(self, unpickler):
+    while True:
+      try:
+        edge = unpickler.load()
+        self.AddEdge(edge)
       except EOFError:
         print "Done reading"
         break
 
   def Dump(self):
-    with open(self.filename, "w") as f:
+    content_to_node_map = dict((x.content, x) for x in self.nodes)
+    with open(self.nodes_filename, "w") as f:
       pickler = pickle.Pickler(f, 2)
       for node in self.nodes:
+        LTM.Mangle(node.content.__dict__, content_to_node_map)
         pickler.dump(node)
+    with open(self.edges_filename, "w") as f:
+      pickler = pickle.Pickler(f, 2)
+      for edge in self.edges:
+        pickler.dump(edge)
 
   def AddNode(self, node):
+    assert(isinstance(node, LTMNode))
     self.nodes.append(node)
+
+  def AddEdge(self, edge):
+    self.edges.append(edge)
--- tests/test_ltm.py
+++ tests/test_ltm.py
@@ -4,21 +4,77 @@ import tempfile
 
 from farg import ltm
 
+class MockCategory(object):
+  def __init__(self, foo):
+    print "Initializing MockCategory instance ", self
+    self.foo = foo
+
+  memos = {}
+  @classmethod
+  def Create(cls, foo):
+    print "Create called with ", foo
+    if foo not in cls.memos:
+      print "New instance generated!!"
+      cls.memos[foo] = cls(foo)
+    return cls.memos[foo]
+
+class MockMapping(object):
+  def __init__(self, category):
+    self.category = category
+
+  memos = {}
+  @classmethod
+  def Create(cls, category):
+    print "Create called with ", category
+    if category not in cls.memos:
+      print "New instance generated!!"
+      cls.memos[category] = cls(category)
+    return cls.memos[category]
+
+class TestLTMNode(unittest.TestCase):
+  def test_sanity(self):
+    node = ltm.LTMNode(MockCategory.Create(3))
+    self.assertEqual(MockCategory.Create(3), node.content)
+
 class TestLTM(unittest.TestCase):
   def setUp(self):
-    filehandle, self.filename = tempfile.mkstemp()
+    filehandle, self.nodes_filename = tempfile.mkstemp()
+    filehandle, self.edges_filename = tempfile.mkstemp()
 
   def tearDown(self):
-    # print "Removing %s" % self.filename
-    os.remove(self.filename)
+    os.remove(self.nodes_filename)
+    os.remove(self.edges_filename)
 
   def test_sanity(self):
-    myltm = ltm.LTM(self.filename)
-    myltm.AddNode(15)
-    myltm.AddNode(17)
+    myltm = ltm.LTM(self.nodes_filename, self.edges_filename)
+    c1 = MockCategory.Create(7)
+    m1 = MockMapping.Create(c1)
+    c2 = MockCategory.Create(9)
+    m2 = MockMapping.Create(c2)
+
+    myltm.AddNode(ltm.LTMNode(c1))
+    myltm.AddNode(ltm.LTMNode(m1))
+    myltm.AddNode(ltm.LTMNode(c2))
+    myltm.AddNode(ltm.LTMNode(m2))
 
     myltm.Dump()
 
-    myltm2 = ltm.LTM(self.filename)
-    self.assertEqual(2, len(myltm2.nodes))
-    self.assertEqual((15, 17), tuple(myltm2.nodes))
+    # I'll remove the memos os MockMapping but not MockCategory, thereby testing the creation
+    # mechanism.
+    MockMapping.memos = {}
+    # MockCategory.memos = {}
+    print "==========================="
+    myltm2 = ltm.LTM(self.nodes_filename, self.edges_filename)
+    self.assertEqual(4, len(myltm2.nodes))
+    c1p, m1p, c2p, m2p = (x.content for x in myltm2.nodes)
+    print 'c1p=', c1p
+    self.assertEqual(7, c1p.foo)
+    self.assertEqual(9, c2p.foo)
+    self.assertEqual(c1p, m1p.category)
+    self.assertEqual(c2p, m2p.category)
+
+    c3 = MockCategory.Create(9)
+    self.assertEqual(c3, c2p)
+
+    m3 = MockMapping.Create(c3)
+    self.assertEqual(m3, m2p)
