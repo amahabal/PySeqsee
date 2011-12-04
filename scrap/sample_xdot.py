1a613639a9458361aa6f5c46569a71d6947e9fb2
Abhijit Mahabal <amahabal@gmail.com>
2011-12-04 13:00:11 -0500

    The toolbar is better...

--- scrap/sample_xdot.py
+++ scrap/sample_xdot.py
@@ -30,7 +30,6 @@ def GetGraph(startnode=None):
         nodes_added.add(other_node)
         lines.append('%s [URL="%s"];' % (other_node, other_node))
       lines.append('%s -> %s;' % (startnode, other_node))
-    lines.append('GoBack [URL="---"];')
     return """
     digraph G {
     %s
@@ -40,21 +39,61 @@ def GetGraph(startnode=None):
 
 
 class MyDotWindow(xdot.DotWindow):
+    ui = '''
+    <ui>
+        <toolbar name="ToolBar">
+            <toolitem action="Reload"/>
+            <toolitem action="FullGraph"/>
+            <toolitem action="GoBack"/>
+            <separator/>
+            <toolitem action="ZoomIn"/>
+            <toolitem action="ZoomOut"/>
+            <toolitem action="ZoomFit"/>
+            <toolitem action="Zoom100"/>
+        </toolbar>
+    </ui>
+    '''
 
     def __init__(self):
         xdot.DotWindow.__init__(self)
+        self.history_stack = []
         self.widget.connect('clicked', self.on_url_clicked)
+        self.uimanager.remove_action_group(self.actiongroup)
+
+        self.actiongroup = gtk.ActionGroup('Actions')
+        self.actiongroup.add_actions((
+            ('FullGraph', gtk.STOCK_HOME, None, None, None, self.display_full_graph),
+            ('GoBack', gtk.STOCK_GO_BACK, None, None, None, self.go_back),
+            ('Reload', gtk.STOCK_REFRESH, None, None, None, self.on_reload),
+            ('ZoomIn', gtk.STOCK_ZOOM_IN, None, None, None, self.widget.on_zoom_in),
+            ('ZoomOut', gtk.STOCK_ZOOM_OUT, None, None, None, self.widget.on_zoom_out),
+            ('ZoomFit', gtk.STOCK_ZOOM_FIT, None, None, None, self.widget.on_zoom_fit),
+            ('Zoom100', gtk.STOCK_ZOOM_100, None, None, None, self.widget.on_zoom_100),
+        ))
+        self.uimanager.insert_action_group(self.actiongroup, 0)
+        self.uimanager.add_ui_from_string(self.ui)
+
 
     def on_url_clicked(self, widget, url, event):
-#        dialog = gtk.MessageDialog(
-#                parent=self,
-#                buttons=gtk.BUTTONS_OK,
-#                message_format="%s clicked" % url)
-#        dialog.connect('response', lambda dialog, response: dialog.destroy())
-#        dialog.run()
-        print GetGraph(url)
+        # print GetGraph(url)
         self.set_dotcode(GetGraph(url))
+        self.history_stack.append(url)
         return True
+
+    def display_full_graph(self, ignored_action):
+      self.set_dotcode(GetGraph(None))
+      self.history_stack = []
+
+    def go_back(self, ignored_action):
+      len_history = len(self.history_stack)
+      if len_history == 0:
+        return
+      if len_history == 1:
+        return self.display_full_graph(ignored_action)
+      prior_url = self.history_stack[-2]
+      self.history_stack = self.history_stack[:-1]
+      self.set_dotcode(GetGraph(prior_url))
+
 
 dotcode = """
 digraph G {
