6d09ba5b0ed678ae0a42c6c7ac6fc2feb74127c8
Abhijit Mahabal <amahabal@gmail.com>
2011-12-27 01:34:40 -0500

    Refactored coderack view.

--- src/apps/seqsee/gui/coderack_view.py
+++ src/apps/seqsee/gui/coderack_view.py
@@ -11,7 +11,8 @@ class CoderackView(ViewPort):
     self.height_per_row = 0.8 * (height - 20) / CoderackView.families_per_page
     self.current_page_number = 1
 
-  def ReDrawView(self, controller):
+  def GetAllItemsToDisplay(self, controller):
+    """Returns a 2-tuple: A top message, and a list of items."""
     coderack = controller.coderack
     families_to_urgency_sum = defaultdict(float)
     families_to_codelet_counts = defaultdict(int)
@@ -20,7 +21,23 @@ class CoderackView(ViewPort):
       families_to_urgency_sum[codelet.family] += codelet.urgency
       families_to_codelet_counts[codelet.family] += 1
       total_codelets += 1
-    families_count = len(families_to_codelet_counts)
+    items = []
+    for family, urgency in families_to_urgency_sum.iteritems():
+      items.append((family, urgency, families_to_codelet_counts[family]))
+    message = '%d codelets from %d families.' % (total_codelets, len(items))
+    return (items, message)
+
+  def DrawItem(self, widget_x, widget_y, item):
+    """Given x, y within the current widget and an item, draws it."""
+    family, urgency, count = item
+    x, y = self.CanvasCoordinates(widget_x, widget_y)
+    self.canvas.create_text(x, y, text='%2d %4.1f %s' % (count, urgency, family),
+                            anchor=NW)
+
+
+  def ReDrawView(self, controller):
+    items, top_message = self.GetAllItemsToDisplay(controller)
+    families_count = len(items)
     max_page_number = ceil(1.0 * families_count / CoderackView.families_per_page)
     if self.current_page_number > max_page_number:
       self.current_page_number = 1
@@ -28,18 +45,11 @@ class CoderackView(ViewPort):
     index_beyond_last_family = index_of_first_family + CoderackView.families_per_page
     if index_beyond_last_family > families_count:
       index_beyond_last_family = families_count
-    families_to_show = families_to_codelet_counts.keys()[index_of_first_family:index_beyond_last_family]
-
-    message = '%d codelets from %d families. Page %d of %d' % (
-        total_codelets, families_count, self.current_page_number, max_page_number)
+    items_to_show = items[index_of_first_family:index_beyond_last_family]
     x, y = self.CanvasCoordinates(0, 0)
-    self.canvas.create_text(x, y, text=message, anchor=NW)
+    self.canvas.create_text(x, y, text=top_message, anchor=NW)
 
     row_top_y = 20
-    for family in families_to_show:
-      count = families_to_codelet_counts[family]
-      urgency = '%4.1f' % families_to_urgency_sum[family]
-      x, y = self.CanvasCoordinates(20, row_top_y)
-      self.canvas.create_text(x, y, text='%2d %s %s' % (count, urgency, family),
-                              anchor=NW)
+    for item in items_to_show:
+      self.DrawItem(20, row_top_y, item)
       row_top_y += self.height_per_row
