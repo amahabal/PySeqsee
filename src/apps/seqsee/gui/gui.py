f369214568af3bbf27fe2a92d2d6590cc0f23780
Abhijit Mahabal <amahabal@gmail.com>
2012-04-12 15:20:26 -0400

    Added license to all files in Seqsee.

--- src/apps/seqsee/gui/gui.py
+++ src/apps/seqsee/gui/gui.py
@@ -10,6 +10,7 @@
 
 # You should have received a copy of the GNU General Public License along with this
 # program.  If not, see <http://www.gnu.org/licenses/>
+
 """Adapting the generic FARG GUI to Seqsee."""
 
 from apps.seqsee.gui.groups_view import GroupsView
