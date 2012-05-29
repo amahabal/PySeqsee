207e311db9d7a8cd04471572c9599e3ae896d312
Abhijit Mahabal <amahabal@gmail.com>
2012-05-29 11:22:54 -0400

    listbox used now for sequences, pie chart and histogram created.

--- farg/core/run_mode/non_interactive.py
+++ farg/core/run_mode/non_interactive.py
@@ -14,7 +14,7 @@
 from farg.core.run_mode.run_mode import RunMode
 from farg.core.run_stats import AllStats
 from farg.third_party import gflags
-from tkinter import Button, Canvas, Frame, LEFT, NW, Text, Tk, TOP
+from tkinter import ACTIVE, BOTH, Button, Canvas, END, Frame, LEFT, Listbox, N, NW, RIGHT, Scrollbar, Tk, TOP, VERTICAL, Y
 import subprocess
 import sys
 import threading
@@ -74,9 +74,16 @@ class MultipleRunGUI:
 
     details_frame = Frame(mw)
     details_frame.pack(side=TOP)
-    #: Text box on left listing inputs.
-    self.text = Text(details_frame)
-    self.text.pack(side=LEFT)
+    #: listbox on left listing inputs.
+    frame = Frame(details_frame)
+    scrollbar = Scrollbar(frame, orient=VERTICAL)
+    listbox = Listbox(frame, yscrollcommand=scrollbar.set, height=25, width=70)
+    scrollbar.config(command=listbox.yview)
+    scrollbar.pack(side=RIGHT, fill=Y)
+    listbox.pack(side=LEFT, fill=BOTH, expand=1)
+    listbox.bind('<ButtonRelease-1>', self.SelectForDisplay)
+    frame.pack(side=LEFT)
+    self.listbox = listbox
     #: Canvas on right for details
     self.canvas = Canvas(details_frame, width=500, height=400, background='#FFFFFF')
     self.canvas.pack(side=LEFT)
@@ -88,6 +95,9 @@ class MultipleRunGUI:
     self.mw.bind('<KeyPress-q>', lambda e: self.Quit())
     self.mw.bind('<KeyPress-r>', lambda e: self.KickOffRun())
     self.Refresher()
+
+  def SelectForDisplay(self, event):
+    self.display_details_for = self.listbox.get(self.listbox.curselection()[0])
 
   def SetupButtonBar(self, mw):
     """Sets up the top button-bar."""
@@ -119,27 +129,29 @@ class MultipleRunGUI:
 
   def UpdateDisplay(self):
     """Displays the Stats."""
-    self.text.delete('0.0', 'end')
+    self.listbox.delete(0, END)
     self.canvas.delete('all')
     inputs = self.stats.input_order
     if not inputs:
       return
-    for input in inputs:
-      left_stats = self.stats.GetLeftStatsFor(input)
-      if not left_stats.IsEmpty():
-        self.text.insert('end', "======%s: %s======\n" % (input, self.stats.left_name))
-        self.text.insert('end', str(left_stats) + "\n")
-      right_stats = self.stats.GetRightStatsFor(input)
-      if not right_stats.IsEmpty():
-        self.text.insert('end', "======%s: %s======\n" % (input, self.stats.right_name))
-        self.text.insert('end', str(right_stats) + "\n")
-    self.display_details_for = inputs[0]  # Hack
+    for idx, input in enumerate(inputs):
+      self.listbox.insert(END, input)
+      if input == self.display_details_for:
+        self.listbox.selection_set(idx)
+#      left_stats = self.stats.GetLeftStatsFor(input)
+#      if not left_stats.IsEmpty():
+#        self.text.insert('end', "======%s: %s======\n" % (input, self.stats.left_name))
+#        self.text.insert('end', str(left_stats) + "\n")
+#      right_stats = self.stats.GetRightStatsFor(input)
+#      if not right_stats.IsEmpty():
+#        self.text.insert('end', "======%s: %s======\n" % (input, self.stats.right_name))
+#        self.text.insert('end', str(right_stats) + "\n")
     if self.display_details_for:
       self.DisplayDetails()
 
   def DisplayDetails(self):
     """Show detailed statistics of one input."""
-
+    self.canvas.create_text(2, 2, text=self.display_details_for, anchor=NW)
     # Display left side
     self.DisplayOneSideStats(y=10, label=self.stats.left_name,
                              stats=self.stats.GetLeftStatsFor(self.display_details_for))
@@ -148,6 +160,47 @@ class MultipleRunGUI:
 
   def DisplayOneSideStats(self, *, y, label, stats):
     self.canvas.create_text(10, y, anchor=NW, text=label)
+    self.CreatePieChart(10, y + 20, stats)
+    self.CreateHistogram(180, y + 20, stats)
+
+  def StateToColor(self, state):
+    if state == b'SuccessfulCompletion':
+      return '#00FF00'
+    if state == b'MaxCodeletsReached':
+      return '#FF0000'
+    print(state)
+    return '#FFFFFF'
+
+  def CreatePieChart(self, x0, y0, stats):
+    stats_per_state = stats.stats_per_state
+    state_to_counts = dict((x, len(y.codelet_counts)) for x, y in stats_per_state.items())
+    total_runs = sum(state_to_counts.values())
+    if total_runs == 0:
+      return
+    start = 0
+    for state, count in state_to_counts.items():
+      extent = 359.9 * count / total_runs
+      color = self.StateToColor(state)
+      self.canvas.create_arc(x0, y0, x0 + 50, y0 + 50, start=start, extent=extent, fill=color)
+      start = start + extent
+    self.canvas.create_text(x0, y0 + 55, anchor=NW, text='%d Runs' % total_runs)
+
+  def CreateHistogram(self, x, y, stats):
+    all_runs = []
+    for state, stats_for_state in stats.stats_per_state.items():
+      all_runs.extend((state, x) for x in stats_for_state.codelet_counts)
+    all_runs = sorted((x for x in all_runs if x[1] > 0), key=lambda x: x[1])
+    count = len(all_runs)
+    if count == 0:
+      return
+    delta_x = 100.0 / count
+    max_codelet_count = max(x[1] for x in all_runs)
+    for idx, run in enumerate(all_runs):
+      color = self.StateToColor(run[0])
+      y_end = y + 100 - 100 * run[1] / max_codelet_count
+      this_x = x + delta_x * idx
+      self.canvas.create_line(this_x, y_end, this_x, y + 100, fill=color)
+    self.canvas.create_text(x + 50, y + 110, anchor=N, text='Max codelets: %d' % max_codelet_count)
 class RunModeNonInteractive(RunMode):
   @classmethod
   def DoSingleRun(cls, cmdline_arguments_dict, extra_arguments=None):
