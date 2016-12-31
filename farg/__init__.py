0674dc1b28e1a963c054c7af1bcf23670888650c
Abhijit Mahabal <amahabal@google.com>
2016-12-30 22:32:46 -0800

    Updated Sphinx config to be compatible with 1.5.1
    Added a note about the debug flags for logging.

--- farg/__init__.py
+++ farg/__init__.py
@@ -1,4 +1,4 @@
-VERSION = (0, 2, 1, 'alpha', 1)
+VERSION = (0, 2, 2, 'alpha', 1)
 def get_version(version=None):
     """Derives a PEP386-compliant version number from VERSION."""
     if version is None:
