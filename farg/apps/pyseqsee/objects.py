4267f69c8aae70e50c7a281a60ba99dcb4ef5efe
Abhijit Mahabal <amahabal@google.com>
2017-01-31 18:02:16 -0800

    Added basics of PSObject, PSGroup, and PSElement

--- farg/apps/pyseqsee/objects.py
+++ farg/apps/pyseqsee/objects.py
@@ -1,4 +1,7 @@
-from farg.core.ltm.storable import LTMNodeContent
+from farg.core.ltm.storable import LTMNodeContent, LTMStorableMixin
+from farg.core.categorization.categorizable import CategorizableMixin
+from farg.core.focusable_mixin import FocusableMixin
+
 
 class PlatonicObject(LTMNodeContent):
   """A stringified representation of a structure---i.e., of possibly nested tuples of integers.
@@ -24,3 +27,49 @@ class PlatonicObject(LTMNodeContent):
       return str(structure)
     assert(isinstance(structure, tuple))
     return '(' + ', '.join(cls._StructureToString(x) for x in structure) + ')'
+
+
+class PSObject(LTMStorableMixin, CategorizableMixin):
+  """Represents an element or group in the workspace.
+
+  This may be anchored or not. When anchored, it has a start and end offset.
+
+  TODO(amahabal): Yet to fill in the notion of anchoring and offsets.
+
+  TODO(amahabal): Have not yet ported over the code for getting the fringe and strength.
+
+  TODO(amahabal): Also not present yet is the storage of relations.
+  """
+
+  def __init__(self):
+    self.is_anchored = False
+
+  def GetStorable(self):
+    return PlatonicObject.CreateFromStructure(self.Structure())
+
+
+class PSElement(PSObject):
+  """Represents a single element in the sequence."""
+
+  def __init__(self, *, magnitude):
+    PSObject.__init__(self)
+    self.magnitude = magnitude
+
+  def Structure(self):
+    return self.magnitude
+
+
+class PSGroup(PSObject):
+  """Represents a group, including the degenerate case of singleton or empty group.
+
+  TODO(amahabal): Not ported over the notion of underlying relations, yet. But maybe what I need is
+  slightly different anyway, since a mountain cannot be cleanly represented just by a single kind
+  of underlying relationship among items.
+  """
+
+  def __init__(self, *, items):
+    PSObject.__init__(self)
+    self.items = items
+
+  def Structure(self):
+    return tuple(x.Structure() for x in self.items)
--- farg/apps/pyseqsee/tests/test_objects.py
+++ farg/apps/pyseqsee/tests/test_objects.py
@@ -1,5 +1,5 @@
 import unittest
-from farg.apps.pyseqsee.objects import PlatonicObject
+from farg.apps.pyseqsee.objects import PlatonicObject, PSElement, PSGroup, PSObject
 from farg.core.ltm.storable import LTMNodeContent
 
 class TestPlatonicObject(unittest.TestCase):
@@ -37,3 +37,26 @@ class TestPlatonicObject(unittest.TestCase):
 
     p6 = PlatonicObject.CreateFromStructure((3, ((4, 5),)))
     self.assertEqual("(3, ((4, 5)))", p6.rep)
+
+class TestPSObject(unittest.TestCase):
+
+  def test_creation(self):
+    # PSElement can be created by passing in magnitude.
+    e1 = PSElement(magnitude=3)
+    self.assertIsInstance(e1, PSElement)
+    self.assertIsInstance(e1, PSObject)
+    self.assertEqual(3, e1.magnitude)
+
+    # It is not memoized.
+    e2 = PSElement(magnitude=3)
+    self.assertNotEqual(e1, e2)
+
+    # It is not anchored.
+    self.assertFalse(e1.is_anchored)
+
+    g1 = PSGroup(items=(e1, e2))
+    self.assertIsInstance(g1, PSGroup)
+    self.assertIsInstance(g1, PSObject)
+
+    self.assertEqual((3,3), g1.Structure())
+    self.assertEqual("(3, 3)", g1.GetStorable().rep)
